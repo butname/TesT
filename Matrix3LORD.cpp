51e47e47f7fb19acd8dfc5c6fece3a82900037f3
butname <132270209+butname@users.noreply.github.com>
2023-07-08 23:05:26 +0700

    Add files via upload

---
+++ Matrix3LORD.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int m, n;
+    cin >> m >> n;
+    vector<int> arr(m * n);
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> arr[i * n + j];
+        }
+    }
+    sort(arr.begin(), arr.end());
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << arr[i * n + j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
